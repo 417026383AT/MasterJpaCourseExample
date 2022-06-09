167836caad0ba74d3644ddc552eb2e8aae1b3cc1
Alejandro Trejo <alejandro.trejo@enroutesystems.com>
2022-06-08 19:17:52 -0500

    Added different types of inheritance JPA

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -17,8 +19,12 @@ import org.hibernate.annotations.GenericGenerator;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+//@MappedSuperclass
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)//CREATES ONE TABLE FOR ALL TYPES OF EMPLOYEE, best performance
+@DiscriminatorColumn(name = "EmployeeType")
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)//CREATES TABLES FOR EACH TYPE OF EMPLOYEE
+@Inheritance(strategy = InheritanceType.JOINED)//CREATES MULTIPLE TABLES, BEST DESIGN best data integrity
 public abstract class Employee {
 
 	@Id
